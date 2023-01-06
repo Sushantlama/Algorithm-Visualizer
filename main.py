83cc0b853ca563f23cfd4b6bd2ceebd35c5ef484
Sushantlama <lsushant009@gmail.com>
2023-01-06 20:42:17 +0530

    from local

---
+++ main.py
@@ -0,0 +1,13 @@
+from snake import Snake
+from tkinter import *
+
+root = Tk(className="python")
+root.resizable(False,False)
+snake = Snake(root)
+root.bind("<KeyPress-Left>", lambda e: snake.left(e))
+root.bind("<KeyPress-Right>", lambda e: snake.right(e))
+root.bind("<KeyPress-Up>", lambda e: snake.up(e))
+root.bind("<KeyPress-Down>", lambda e: snake.down(e))
+root.mainloop()
+
+
